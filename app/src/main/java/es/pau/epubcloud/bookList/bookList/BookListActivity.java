40b1e6c411b137960aec5112c058bf79be3cb8b6
Gensin <ghostdesert@gmail.com>
2016-01-16 13:10:38 +0100

    Added dropbox authentication

--- app/src/main/java/es/pau/epubcloud/bookList/bookList/BookListActivity.java
+++ app/src/main/java/es/pau/epubcloud/bookList/bookList/BookListActivity.java
@@ -1,10 +1,16 @@
 package es.pau.epubcloud.bookList.bookList;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
 import com.dropbox.client2.DropboxAPI;
 import com.dropbox.client2.android.AndroidAuthSession;
+import com.dropbox.client2.session.AccessTokenPair;
+import com.dropbox.client2.session.AppKeyPair;
 
 import es.pau.epubcloud.R;
 
@@ -26,8 +32,6 @@ public class BookListActivity extends AppCompatActivity {
     private static final String ACCESS_KEY_NAME = "ACCESS_KEY";
     private static final String ACCESS_SECRET_NAME = "ACCESS_SECRET";
 
-    private static final boolean USE_OAUTH1 = false;
-
     DropboxAPI<AndroidAuthSession> mApi;
 
     private boolean mLoggedIn;
@@ -37,6 +41,80 @@ public class BookListActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Creamos una nueva sesión the autenticación con dropbox API
+        AndroidAuthSession session = buildSession();
+        mApi = new DropboxAPI<AndroidAuthSession>(session);
+
+        // Si no estamos ya conectados, abrimos una sesión de login
+        if (!mLoggedIn) {
+            mApi.getSession().startOAuth2Authentication(BookListActivity.this);
+        }
+        // El login se resuelve en el onResume
+
+        //
         setContentView(R.layout.activity_book_list);
+
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        AndroidAuthSession session = mApi.getSession();
+
+        if (session.authenticationSuccessful()) {
+            try {
+                // Completamos la autenticacion
+                session.finishAuthentication();
+
+                // Guardamos la autenticacion
+                storeAuth(session);
+                mLoggedIn = true;
+            } catch (IllegalStateException e) {
+                showToast(getResources().getString(R.string.dropbox_fail) + e.getLocalizedMessage());
+            }
+        }
+    }
+
+    private void showToast(String msg) {
+        Toast error = Toast.makeText(this, msg, Toast.LENGTH_LONG);
+        error.show();
+    }
+
+
+    /*
+     * Guardamos el token de autenticacion del usuario para mantener la sesión abierta
+     */
+    private void storeAuth(AndroidAuthSession session) {
+        //  Si hay token de lo guardamos
+        String oauth2AccessToken = session.getOAuth2AccessToken();
+        if (oauth2AccessToken != null) {
+            SharedPreferences prefs = getSharedPreferences(ACCOUNT_PREFS_NAME, 0);
+            SharedPreferences.Editor edit = prefs.edit();
+            edit.putString(ACCESS_KEY_NAME, "oauth2:");
+            edit.putString(ACCESS_SECRET_NAME, oauth2AccessToken);
+            edit.apply();
+        }
+    }
+
+    /**
+     * Cargamos el token de autenticacion si este ya existia
+     */
+    private void loadAuth(AndroidAuthSession session) {
+        SharedPreferences prefs = getSharedPreferences(ACCOUNT_PREFS_NAME, 0);
+        String key = prefs.getString(ACCESS_KEY_NAME, null);
+        String secret = prefs.getString(ACCESS_SECRET_NAME, null);
+        if (key == null || secret == null || key.length() == 0 || secret.length() == 0) return;
+        session.setOAuth2AccessToken(secret);
+
+    }
+
+    private AndroidAuthSession buildSession() {
+        AppKeyPair appKeyPair = new AppKeyPair(APP_KEY, APP_SECRET);
+
+        AndroidAuthSession session = new AndroidAuthSession(appKeyPair);
+        loadAuth(session);
+        return session;
     }
 }
